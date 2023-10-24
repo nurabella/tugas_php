6da384b8b332b55c0583589a289056d5038ed360
nurabella <nurabella452@gmail.com>
2023-10-24 21:26:52 +0700

    sesi28

--- sesi28.php
+++ sesi28.php
@@ -1,70 +1,92 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sesi 28</title>
+	<title>sesi 28</title>
 </head>
 <body>
-<form>
-		<table border="1" >
+
+		<table border="1">
 			<tr>
-				<th>No</th>
-                <th>Nama</th>
-                <th>Tanggal Lahir</th>
-				<th>Umur</th>
+				<th>NO</th>
+				<th>Nama</th>
+				<th>Tanggal lahir</th>
+                <th>Umur</th>
                 <th>Alamat</th>
                 <th>Kelas</th>
                 <th>Nilai</th>
                 <th>Hasil</th>
 			</tr>
- 
-		<?php    
-        $data =file_get_contents ("data.json");
-        $students = json_decode($data);
-    
-        foreach ($students as $student)	
-        ?>
-        
-            <tr>
-                <td><?php ?></td>
+
+            <?php
+            $data = file_get_contents("data.json");
+            $students = json_decode($data);
+            $no = 1;
+            
+            foreach ($students as $student){
+                if($no % 2 == 0){
+                    $warna='#fff';
+                }
+                else{
+                    $warna='#ccc';
+                }
+            ?>
+
+            <tr bgcolor= "<?php echo $warna ?>">
+                <td><?php echo $no;?></td>
                 <td>
-                    <?php
-                    echo $student->nama. "<br>";
+                    <?php 
+                        echo $student->nama;
                     ?>
                 </td>
                 <td>
-                    <?php
-                    echo $student->tanggal_lahir. "<br>";
+                    <?php 
+                        echo $student->tanggal_lahir;
                     ?>
-                    
                 </td>
                 <td>
-                    <?php
-                    echo $student->umur. "<br>";
+                    <?php 
+                        $lahir = new DateTime($student->tanggal_lahir);
+                        $today        =new DateTime();
+                        $umur = $today->diff($lahir);
+                        echo $umur->y;
                     ?>
-                    
                 </td>
                 <td>
-                    <?php
-                    echo $student->alamat. "<br>";
+                    <?php 
+                        echo $student->alamat;
                     ?>
-                    
                 </td>
                 <td>
-                    <?php
-                    echo $student->kelas. "<br>";
+                    <?php 
+                        echo $student->kelas;
                     ?>
-                    
                 </td>
                 <td>
-                    <?php
-                    echo $student->nilai. "<br>";
+                    <?php 
+                        echo $student->nilai;
                     ?>
-                    
                 </td>
+                <td>
+                    <?php 
+                    $nilai=$student->nilai;
+                        if ($nilai <= 59) {
+                            echo "E";
+                        } else if ($nilai >= 60 && $nilai <= 69) {
+                            echo "D";
+                        } else if ($nilai >= 70 && $nilai <= 79) {
+                            echo "C";
+                        } else if ($nilai >= 80 && $nilai <= 89) {
+                            echo "B";
+                        } else if ($nilai >= 90 && $nilai <= 100) {
+                            echo "A";
+                        } 
+                    ?>
+                </td>
+                
             </tr>
-      
+            <?php $no++;}
+            ?>
+ 
 		</table>
 	
 </body>
