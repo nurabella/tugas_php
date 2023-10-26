0676169494042ef58407450c106acc91cfba1c7d
nurabella <nurabella452@gmail.com>
2023-10-26 21:53:15 +0700

    add sesi 28 new

--- sesi28.php
+++ sesi28.php
@@ -5,13 +5,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 </head>
+<style>
+    
+</style>
+
 <body>
     <nav class="navbar bg-warning">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Daftar Nilai</a>
-    </div>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Daftar Nilai</a>
+        </div>
     </nav>
-		<table border="1">
+    <br>
+    <table class="table table-bordered" cellspacing="0" align="center" style="width: 700px;">
 			<tr>
 				<th>No</th>
 				<th>Nama</th>
@@ -94,7 +99,6 @@
             ?>
  
 		</table>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
