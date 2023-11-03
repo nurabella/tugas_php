f62ce25cd62cc999fae09378f72d0479749e8e3d
nurabella <nurabella452@gmail.com>
2023-11-03 19:48:43 +0700

    kuis php

---
+++ sesi33.php
@@ -0,0 +1,32 @@
+<?php
+//No 1
+$array = array("satu", "dua", "tiga","empat", "lima");
+
+for($a=4; $a>=0; $a--){
+    echo "$array[$a] <br>";
+}
+
+
+//No 2
+$a = array("apel", "nanas", "mangga","jeruk");
+
+echo  "<br>" .count($a);
+
+//No 3
+
+    $aray=array(7,3,4,9);
+    $jumlah=array_sum($aray);
+
+    echo "7+3+4+9 = $jumlah <br>";
+
+//No 4
+for($a=1;$a<=1;$a++)
+    {
+        for($b=1;$b<=10;$b++)
+        {
+        $total=$a*$b;
+        echo "<p> $a x $b=$total<br><br>";
+        }
+    }
+
+?>
